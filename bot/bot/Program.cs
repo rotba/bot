40fb35675bc1e79425b0fcc36ed1d193dabd20e3
Rotem <rotba@post.bgu.ac.il>
2018-12-25 17:11:39 +0200

    nop

--- bot/bot/Program.cs
+++ bot/bot/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,14 @@ namespace bot
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            IPAddress newaddress = IPAddress.Parse("192.168.1.1");
+            IPEndPoint ipep =new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                              SocketType.Stream, ProtocolType.Tcp);
+            server.Connect(ipep);
+            byte[] buffer = Encoding.ASCII.GetBytes("INBI");
+            server.Send(buffer, 4, SocketFlags.None);
+            while (true) { }
         }
     }
 }
