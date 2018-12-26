87a76c0d439ab6671cd17fc3415c06b13eff939e
Rotem <rotba@post.bgu.ac.il>
2018-12-27 00:54:58 +0200

    before meging with hadas

--- bot/bot/Program.cs
+++ bot/bot/Program.cs
@@ -12,8 +12,7 @@ namespace bot
     {
         static void Main(string[] args)
         {
-            IPAddress newaddress = IPAddress.Parse("192.168.1.1");
-            IPEndPoint ipep =new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            IPEndPoint ipep =new IPEndPoint(IPAddress.Parse("172.16.15.50"), 8000);
             Socket server = new Socket(AddressFamily.InterNetwork,
                               SocketType.Stream, ProtocolType.Tcp);
             server.Connect(ipep);
